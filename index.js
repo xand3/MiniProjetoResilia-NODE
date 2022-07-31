7c0d5a521454458e1aac57ce181726cd2c28d28c
Alexandre Bastos <xande.bastos321@gmail.com>
2022-07-31 17:54:17 -0300

    Finalizando projeto

--- index.js
+++ index.js
@@ -5,19 +5,23 @@ function organiza() {
     propriedades.pop();
     propriedades.sort();
 }
+function instrucoes() {
+    console.log("=============================");
+    console.log("DIGITE AS PROPRIEDADES CSS");
+    console.log("para ver o resultado digite SAIR");
+}
 
 let propriedades = [];
 let resposta = "";
-console.log("=============================");
-console.log("DIGITE AS PROPRIEDADES CSS");
-console.log("para ver o resultado digite SAIR");
-
+instrucoes()
 for (let i = 0; resposta != "SAIR"; i++) {
     resposta = readLineSync.question(">");
     propriedades[i] = resposta;
 }
+
 organiza()
 
 for (let j = 0; j < propriedades.length; j++) {
     console.log(propriedades[j]);
 }
+
